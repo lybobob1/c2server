ae053d54e9f5b4fa1ad98f536a98a4bf67670692
= <=>
2020-06-18 16:09:37 -0400

    added agents folder

---
+++ c2server/agents/python/agent.py
@@ -0,0 +1,22 @@
+# from time import sleep
+# import random
+
+# cburl = "http://127.0.0.1/callback"
+
+
+# def httpGet():
+
+
+
+# sleep_min = 0 #by default sleep anywhere between 0 and 60 seconds
+# sleep_max = 60
+
+
+
+
+# while(True):
+#     random.seed()
+#     interval = random.randint(sleep_min, sleep_max)
+#     sleep(interval)
+
+#leaving this as a placeholder
